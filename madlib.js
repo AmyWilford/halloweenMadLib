a7c59c5f519c4774712627897580a00e3484dcc8
Amy Wilford <wilford.amy@gmail.com>
2021-10-29 18:08:35 -0400

    number verification complete

--- madlib.js
+++ madlib.js
@@ -1,6 +1,7 @@
 let storyInput = document.querySelector('#storyInput');
 let storyOutput = document.querySelector('#storyOutput');
 let warning = document.querySelector('#warning');
+let numberWarning = document.querySelector('#numberWarning');
 let creature;
 
 
@@ -13,24 +14,28 @@ let qty = document.querySelector('#qty')
 let theStory = document.querySelector('#theStory')
 
 let submit = document.querySelector('#submit');
-submit.addEventListener("click", writeStory);
+submit.addEventListener("click", writeStory, false);
 
 document.querySelector('#myName').focus();
-window.addEventListener('keydown', keydownHandler);
+window.addEventListener('keydown', keydownHandler, false);
 
 
 const again = document.querySelector('#again');
-again.addEventListener('click', resetPage);
+again.addEventListener('click', resetPage, false);
 
  storyInput.className= "displayBlock";
  storyOutput.className = "displayNone";
  warning.className = "displayNone";
+ numberWarning.className="displayNone";
 
  myName.focus(); 
 
 function checkComplete() {
-    if(myName.value =='' || myCostume.value== '' || familyMember.value =='' || familyCostume.value =='' || qty.value =='') {
+    if(myName.value =='' || myCostume.value== '' || familyMember.value =='' || familyCostume.value =='' || qty.value ==''){
         warning.className = 'displayBlock';
+        return false
+    } else if (qty.value > 5 || qty.value < 0) {
+        numberWarning.className='displayBlock';
         return false
     } else {
         return true;
@@ -38,13 +43,14 @@ function checkComplete() {
     // this function needs to return true or false to make sure that it can be used in the writeStory function which is checking for a boolean value
 }
 
+
 //  function to create write story with inputted answers
 function writeStory(){
     if(checkComplete() === false) {
         return 
     }
     let finishedStory = '';
-    finishedStory+= 'Tonight is the night when all of the <span id="spookyOrange">spooky Monsters </span>come out to play. <br>My name is <span class="specialValue">' + myName.value + ' </span> and this year I am going trick-or-treating dressed as a <span class="specialValue">' + myCostume.value + '</span>. <span class="specialValue">' + familyMember.value + ' </span> is going with me, dressed as a <span class="specialValue">' + familyCostume.value + '</span>. <br>I hope we see <span id="spookyValue">' + qty.value + ' ' + pickCreature(qty.value) + ' </span> tonight. <br>I will wish them a <br><span id="halloweenValue">HAPPY HALLOWEEN!</span>'
+    finishedStory+= 'Tonight is the night when all of the <span class="spookyValue">spooky Monsters </span>come out to play. <br>My name is <span class="specialValue">' + myName.value + ' </span> and this year I am going trick-or-treating dressed as a <span class="specialValue">' + myCostume.value + '</span>. <span class="specialValue">' + familyMember.value + ' </span> is going with me, dressed as a <span class="specialValue">' + familyCostume.value + '</span>. <br>I hope we see <span class="spookyValue">' + qty.value + ' ' + pickCreature(qty.value) + ' </span> tonight. <br>I will wish them a <br><span id="halloweenValue">HAPPY HALLOWEEN!</span>'
     theStory.innerHTML = finishedStory;
 };
 
@@ -57,6 +63,7 @@ function keydownHandler(event) {
         storyInput.className= "displayNone";
         storyOutput.className = "displayBlock";
         warning.className = "displayNone";
+        numberWarning.className = "displayNone";
         body.style.backgroundColor = 'black';
     }
  }
@@ -89,6 +96,7 @@ function resetPage() {
     storyInput.className= "displayBlock";
     storyOutput.className = "displayNone";
     warning.className = "displayNone";
+    numberWarning.className = "displayNone";
     body.style.backgroundColor = 'white';
     body.style.color = '#26003b';
 
